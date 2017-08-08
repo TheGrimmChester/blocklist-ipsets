19ebce285b2f4ccdb43bfc10c3f015d2da6581ca
Costa Tsaousis <costa@tsaousis.gr>
2017-08-08 22:28:45 +0100

    Tue Aug  8 21:28:45 UTC 2017 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -390,7 +390,7 @@
 [ -f 'dronebl_auto_botnets.netset' ] && /usr/bin/touch --date=@1502221214 'dronebl_auto_botnets.netset'
 [ -f 'dronebl_autorooting_worms.netset' ] && /usr/bin/touch --date=@1499815518 'dronebl_autorooting_worms.netset'
 [ -f 'dronebl_compromised.netset' ] && /usr/bin/touch --date=@1502044813 'dronebl_compromised.netset'
-[ -f 'dronebl_ddos_drones.netset' ] && /usr/bin/touch --date=@1502227213 'dronebl_ddos_drones.netset'
+[ -f 'dronebl_ddos_drones.netset' ] && /usr/bin/touch --date=@1502227523 'dronebl_ddos_drones.netset'
 [ -f 'dronebl_dns_mx_on_irc.netset' ] && /usr/bin/touch --date=@1501968612 'dronebl_dns_mx_on_irc.netset'
 [ -f 'dronebl_irc_drones.netset' ] && /usr/bin/touch --date=@1502217314 'dronebl_irc_drones.netset'
 [ -f 'dronebl_unknown.netset' ] && /usr/bin/touch --date=@1498184408 'dronebl_unknown.netset'
